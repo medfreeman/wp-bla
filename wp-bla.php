4db96bf9847a6ed56d75735ed6e23f26c84c9fff
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-08-09 13:07:08 +0200

    removed eslint and gulp, moved phpcs config to phpcs.xml, changed phpcs config, php code compliance

--- wp-bla.php
+++ wp-bla.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 Plugin Name: Bla
 Plugin URI: https://github.com/medfreeman/wp-bla
 Description: Provides an interface for maintaining voxusini monthly blas
@@ -9,31 +9,37 @@ Author URI: https://github.com/medfreeman
 Author Email: mehdi.lahlou@free.fr
 License: GPLv3
 
-  Copyright 2016 Mehdi Lahlou (mehdi.lahlou@free.fr)
+@package wp-bla
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as
-  published by the Free Software Foundation.
+	Copyright 2016 Mehdi Lahlou (mehdi.lahlou@free.fr)
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-*/
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
+/**
+ * Main class.
+ */
 class Bla {
 
 	const POST_TYPE = 'bla';
 	const LANG_PREFIX = 'bla';
 
-	/*--------------------------------------------*
+	/*
+	 * --------------------------------------------
 	 * Constructor
-	 *--------------------------------------------*/
+	 * --------------------------------------------
+	 */
 
 	/**
 	 * Initializes the plugin by setting localization, filters, and administration functions.
@@ -43,16 +49,16 @@ class Bla {
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
 
-		// Load plugin text domain
+		// Load plugin text domain.
 		add_action( 'init', array( $this, 'plugin_textdomain' ) );
-		// Register post type
+		// Register post type.
 		add_action( 'init', array( $this, 'register_post_type_bla' ) );
 	} // end constructor
 
 	/**
 	 * Fired when the plugin is activated.
 	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog.
 	 */
 	public function activate( $network_wide ) {
 		$this->register_post_type_bla();
@@ -62,7 +68,7 @@ class Bla {
 	/**
 	 * Fired when the plugin is deactivated.
 	 *
-	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog
+	 * @param	boolean	$network_wide	True if WPMU superadmin uses "Network Activate" action, false if WPMU is disabled or plugin is activated on an individual blog.
 	 */
 	public function deactivate( $network_wide ) {
 		flush_rewrite_rules();
@@ -77,6 +83,9 @@ class Bla {
 
 	} // end plugin_textdomain
 
+	/**
+	 * Registers post type bla.
+	 */
 	public function register_post_type_bla() {
 		register_post_type( self::POST_TYPE, array(
 			'label' => __( 'Blas', self::LANG_PREFIX ),
