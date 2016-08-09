3b711c4d61e70ea3bb56634b1421a79ad22c1f1d
Mehdi Lahlou <mehdi.lahlou@free.fr>
2016-08-09 13:08:07 +0200

    version bump to 1.2.0

--- wp-bla.php
+++ wp-bla.php
@@ -4,7 +4,7 @@ Plugin Name: Bla
 Plugin URI: https://github.com/medfreeman/wp-bla
 GitHub Plugin URI: https://github.com/medfreeman/wp-bla
 Description: Provides an interface for maintaining voxusini monthly blas
-Version: 1.1.1
+Version: 1.2.0
 Author: Mehdi Lahlou
 Author URI: https://github.com/medfreeman
 Author Email: mehdi.lahlou@free.fr
